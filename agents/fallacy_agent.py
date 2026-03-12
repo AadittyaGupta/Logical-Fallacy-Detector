12c0e62964dbed5acd3e36a8cad57c824eac6925
AadittyaGupta <guptadi558@gmail.com>
2026-03-12 10:22:41 +0530

    Updated Fallacy agent logic

--- agents/fallacy_agent.py
+++ agents/fallacy_agent.py
@@ -1,38 +1,3 @@
-# from prompt.fallacy_prompt import Prompt_Template
-# from llm.llm_client import call_llm
-# from utils.parser import parse_response
-
-
-
-# class LogicalFallacyAgent:
-#     """
-#     Agent that analyzes an argument for logical fallacies 
-#     """
-
-#     def analyzer(self, argument):
-#         """
-#         Analyze a single argument and return a structured information
-#         """
-
-#         # Prepare prompt for the LLM by inserting the argument
-#         prompt = Prompt_Template.format(argument=argument)
-
-#         # Call the llm to get the required anlysis
-#         response = call_llm(prompt)
-
-#         # parsinf the llm response into structure fields
-#         parsed = parse_response(response)
-
-
-#         return parsed
-
-
-
-
-
-
-
-
 
 from prompt.fallacy_prompt import Prompt_Template
 from llm.llm_client import call_llm
